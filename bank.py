7dc18bd0239a98e402de76afe061c85d3417c83a
test <test>
2023-10-16 11:26:46 +0700

    test

---
+++ bank.py
@@ -0,0 +1,9 @@
+def main():
+     x=input("Приветствие: ")
+     if x.startswith ("здравствуйте"):
+         print ("$0")
+     elif x.startswith ("з"):
+          print ("$20")
+     else:
+          print ("$100")
+main()
