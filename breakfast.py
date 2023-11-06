ac52038480f7dec5a3edf62719007e5d1dc75d43
test <test>
2023-11-06 13:02:14 +0700

    test

---
+++ breakfast.py
@@ -0,0 +1,27 @@
+
+menu = {
+   "кофе":20.00,
+   "чай":10.00,
+   "булочка":5.00,
+   "салат":30.40,
+   "пирожное":45.50,
+}
+
+
+amount = 0
+
+while True:
+    try:
+        x = input("Блюдо: ")
+        if x in menu:
+            amount += menu[x]
+
+
+    except EOFError:
+           break
+
+
+print(f"\nCумма: {amount:.2f}")
+
+
+
